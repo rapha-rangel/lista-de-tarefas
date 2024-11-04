9b03778ccc17c5a900a073020b4ccbd3e39ec992
Raphael Rangel <rapha.carvalho7@gmail.com>
2024-11-04 18:05:05 -0300

    tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,20 +10,6 @@ module.exports = {
         'auto-fill-auto': 'repeat(auto-fill, 120px)',
       },
     },
-    keyframes: {
-      fadeInModal: {
-        '0%': { display:"none",position:"fixed", opacity:"0", transform:"translateX(-40%) translateY(-50%)" },
-        '100%': { position:"fixed", opacity:"1",transform:"translateX(-50%) translateY(-50%)"  },
-      },
-      fadeOutModal: {
-        '0%': { position:"fixed", opacity:"1",transform:"translateX(-50%) translateY(-50%)" },
-        '100%': { display:"none",position:"fixed", opacity:"0", transform:"translateX(-40%) translateY(-50%)" },
-      },
-    },
-    animation: {
-      animateOutModal: 'fadeOutModal 1s ease-in-out forwards',
-      animateInModal: 'fadeInModal 2s ease-in-out forwards '
-    },
   },
   plugins: [],
 }
